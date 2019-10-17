a59203cefbf8a1decf565e7c68cee19a24c064ec
Quentin PERROT-MINNOT <qperrot-@e3r9p4.42.fr>
2019-10-17 14:21:31 +0200

    new strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: qperrot- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/07 15:49:05 by qperrot-          #+#    #+#             */
-/*   Updated: 2019/10/17 12:07:28 by qperrot-         ###   ########.fr       */
+/*   Updated: 2019/10/17 14:17:08 by qperrot-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	str2 = (unsigned char*)s2;
 	if (n == 0)
 		return (0);
-	while (i < n && str1[i] && str2[i])
+	while (--n && str1[i] && str2[i])
 	{
 		if (str1[i] != str2[i])
 			return (str1[i] - str2[i]);
